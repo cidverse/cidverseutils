36027a5f0eab9e34adc736a971b48a63daccbd4e
Philipp Heuer <git@philippheuer.me>
2021-06-12 01:32:39 +0200

    feat: ToUnixPath now also handles A:/ paths correctly

--- pkg/cihelper/platform.go
+++ pkg/cihelper/platform.go
@@ -57,7 +57,10 @@ func ToUnixPath(path string) string {
 	driveLetters := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 	for _, element := range driveLetters {
 		if strings.HasPrefix(path, element+":\\") {
-			path = strings.Replace(path, element+":\\", "/"+element+"/", 1)
+			path = strings.Replace(path, element+":\\", "/"+element+"/", -1)
+			path = strings.Replace(path, "\\", "/", -1)
+		} else if strings.HasPrefix(path, element+":/") {
+			path = strings.Replace(path, element+":/", "/"+element+"/", -1)
 		}
 	}
 
@@ -70,21 +73,17 @@ func ToUnixPath(path string) string {
 // ToUnixPathArgs checks each argument and turns it into a unix path if needed
 func ToUnixPathArgs(data string) string {
 	argList := strings.Split(data, " ")
-	isWindowsPath := false
 
 	for _, a := range argList {
 		driveLetters := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 		for _, element := range driveLetters {
 			if strings.HasPrefix(a, element+":\\") {
-				data = strings.Replace(data, element+":\\", "/"+element+"/", 1)
-				isWindowsPath = true
+				data = strings.Replace(data, element+":\\", "/"+element+"/", -1)
+				data = strings.Replace(data, "\\", "/", -1)
+			} else if strings.HasPrefix(a, element+":/") {
+				data = strings.Replace(data, element+":/", "/"+element+"/", -1)
 			}
 		}
-	}
-
-	// replace windows path separator with linux path separator
-	if isWindowsPath {
-		data = strings.Replace(data, "\\", "/", -1)
 	}
 
 	return data
--- pkg/cihelper/platform_test.go
+++ pkg/cihelper/platform_test.go
@@ -15,3 +15,17 @@ func TestCiEnvironmentFalse(t *testing.T) {
 	_ = os.Unsetenv("CI")
 	assert.Equal(t, false, IsCIEnvironment())
 }
+
+func TestToUnixPath(t *testing.T) {
+	args := "H:/example3/test"
+	unixPath := ToUnixPath(args)
+
+	assert.Equal(t, "/H/example3/test", unixPath)
+}
+
+func TestToUnixPathArgs(t *testing.T) {
+	args := "H:\\example1 H:\\example2 H:/example3/test"
+	unixPath := ToUnixPathArgs(args)
+
+	assert.Equal(t, "/H/example1 /H/example2 /H/example3/test", unixPath)
+}
